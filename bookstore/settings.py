2ee3843a689f23ea92876334abe2164b13e8c228
Evaldoer <evaldorode@gmail.com>
2025-12-11 20:56:21 -0300

    Salva alterações antes de trocar de branch

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = "bookstore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "bookstore_dev_db"),
+        "USER": os.environ.get("POSTGRES_USER", "bookstore_dev"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "bookstore_dev"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
