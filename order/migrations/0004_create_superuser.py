f10a57e95ee47899cf077d5e296dda4548b8aca6
Evaldoer <evaldorode@gmail.com>
2025-12-15 02:21:54 -0300

    Fix migration order and create superuser

--- order/migrations/0004_create_superuser.py
+++ order/migrations/0004_create_superuser.py
@@ -2,17 +2,17 @@ from django.db import migrations
 from django.contrib.auth.models import User
 
 def create_superuser(apps, schema_editor):
-    if not User.objects.filter(username="admin").exists():
+    if not User.objects.filter(username="evaldo").exists():
         User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
+            username="evaldo",
+            email="evaldo.dev2025@gmail.com",
             password="admin123"
         )
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('order', '0001_initial'),
+        ('order', '0003_remove_order_quantity'),
     ]
 
     operations = [
